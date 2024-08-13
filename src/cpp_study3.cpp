01c3f1ebc37e2314f20c21ceab43e6b22495e36b
jayiuk <jayiuk987@gmail.com>
2024-08-13 10:40:27 +0900

    cpp study dec, oct, hex transformation

---
+++ src/cpp_study3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    cout << "숫자 10을 10진수로 표현하면" << a << "임" << endl;
+    cout << oct;
+    cout << "8진수로 표현하면" << a << "임" << endl;
+    cout << hex;
+    cout << "16진수로 표현하면" << a << "임";
+    return 0;
+}
