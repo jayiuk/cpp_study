4ee311022812653a4c1c9cf937c41356a6f58753
jayiuk <jayiuk987@gmail.com>
2024-08-14 10:03:33 +0900

    cpp study increment and decrement operator

--- src/cpp_study11.cpp
+++ src/cpp_study11.cpp
@@ -5,16 +5,20 @@ int main(){
     int num2 = 8;
     int num3 = 8;
     int num4 = 8;
-    int result1, result2, result3, result4;
+    int result1, result2, result3, result4, result5, result6;
 
     result1 = --num1 + 5;
     result2 = num2-- + 5;
     result3 = ++num3 + 5;
     result4 = num4++ + 5;
+    result5 = num4 + 5;
+    result6 = num2 + 5;
 
     cout << "result1 : " << result1 << endl;
     cout << "result2 : " << result2 << endl;
     cout << "result3 : " << result3 << endl;
-    cout << "result4 : " << result4;
+    cout << "result4 : " << result4 << endl;
+    cout << "result5 : " << result5 << endl;
+    cout << "result6 : " << result6 << endl;
     return 0;
 }
