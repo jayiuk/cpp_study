e7d104b52d9e3c023f8b93de56c6ae1a74e1a069
jayiuk <jayiuk987@gmail.com>
2024-08-14 11:13:23 +0900

    comparison operator

---
+++ src/cpp_study12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 3;
+    int num2 = 7;
+
+    cout << "!= result : " << (num1 != num2) << endl;
+    cout << ">= result : " << (num1 >= num2) << endl;
+    return 0;
+}
