5d20a460abaf257eca5a29fcf3ca90ea3d5e9206
jayiuk <jayiuk987@gmail.com>
2024-08-14 09:17:03 +0900

    cpp study arithmetic operator

---
+++ src/cpp_study9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 10;
+    int num2 = 4;
+    cout << "+ 연산자에 의한 결과 : " << num1 + num2 << endl;
+    cout << "- 연산자에 의한 결과 : " << num1 - num2 << endl;
+    cout << "* 연산자에 의한 결과 : " << num1 * num2 << endl;
+    cout << "/ 연산자에 의한 결과 : " << num1 / num2 << endl;
+    cout << "% 연산자에 의한 결과 : " << num1 % num2 << endl;
+    return 0;
+}
